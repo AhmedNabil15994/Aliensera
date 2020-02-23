3fd77a2279909b93f31d8722b8abf0008685d963
Ahmed Nabil <mody998866@gmail.com>
2020-02-23 22:52:23 +0200

    Fix

--- backend/app/Modules/Courses/Models/Course.php
+++ backend/app/Modules/Courses/Models/Course.php
@@ -79,10 +79,12 @@ class Course extends Model{
             if($mainDuration != 0){
                 $rate = round( ($student_duration / $mainDuration) * 100 ,2);
                 $index = floor($rate / 10);
-                if($index >= 10){
-                    $index = 10;
+                if($index > 0){
+                    if($index >= 10){
+                        $index = 10;
+                    }
+                    $mainViews[$index-1] = $mainViews[$index-1] + 1; 
                 }
-                $mainViews[$index-1] = $mainViews[$index-1] + 1; 
 
             }
         }
