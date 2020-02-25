6b29aa8ed53b28d0955518d1e7a9e1320bb45751
Ahmed Nabil <mody998866@gmail.com>
2020-02-25 16:54:52 +0200

    Fix

--- backend/app/Modules/Lessons/Models/LessonVideo.php
+++ backend/app/Modules/Lessons/Models/LessonVideo.php
@@ -72,8 +72,8 @@ class LessonVideo extends Model{
     static function getDuration($duration){
         $result = '';
         if($duration > 3600){
-            $hours = round($duration / 60);
-            $minutes = $duration % 60;
+            $hours = round($duration / 3600);
+            $minutes = round(($duration % 3600) / 60);
             $result = $hours.' Hr '.$minutes.' Min';
         }elseif($duration > 60){
             $minutes = round($duration / 60);
