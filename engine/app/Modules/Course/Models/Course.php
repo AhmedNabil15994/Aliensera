94cddd32dfa477273f7ea8a00a4e94496a59a9ba
Ahmed Nabil <mody998866@gmail.com>
2020-06-30 23:18:57 +0200

    Fix

--- engine/app/Modules/Course/Models/Course.php
+++ engine/app/Modules/Course/Models/Course.php
@@ -72,7 +72,8 @@ class Course extends Model{
 
     static function dataList($type=null,$counter=null) {
         $input = \Input::all();
-
+        //->whereIn('status',[3,5])
+        dd(USER_ID);
         $source = self::NotDeleted()->with('Feedback')->where(function($whereQuery){
             $whereQuery->where('valid_until',null)->orWhere('valid_until','>=',date('Y-m-d'));
         });
