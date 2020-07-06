279eb5e98800fd6e6cf711ffdbcf4149e4e8dc8f
Ahmed Nabil <mody998866@gmail.com>
2020-07-07 01:54:36 +0200

    Fix

--- backend/app/Modules/Courses/Views/view.blade.php
+++ backend/app/Modules/Courses/Views/view.blade.php
@@ -91,7 +91,7 @@
                         <br />
                     </div>
 
-                    <div class="col-md-12">
+                    <div class="col-xs-12">
                         <div class="" role="tabpanel" data-example-id="togglable-tabs">
                             <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
                                 <li role="presentation" class="active"><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">Lectures</a></li>
