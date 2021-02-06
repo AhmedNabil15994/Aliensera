522f1a6d4dbb9971b7813b5f2090a88c077c029f
Ahmed Nabil <mody998866@gmail.com>
2021-02-06 15:31:31 +0200

    Fix

--- backend/app/Modules/Users/Models/User.php
+++ backend/app/Modules/Users/Models/User.php
@@ -52,7 +52,7 @@ class User extends Model{
                 if (isset($input['name']) && !empty($input['name'])) {
                     $queryProfile->where('first_name', 'LIKE', '%' . $input['name'] . '%')
                         ->orWhere('last_name', 'LIKE', '%' . $input['name'] . '%')
-                        ->orWhere('id',$input['name']);
+                        ->orWhere('id','=',(int)$input['name']);
                 }
                 if (isset($input['group_id']) && $input['group_id'] != 0) {
                     $queryProfile->where('group_id', $input['group_id']);
